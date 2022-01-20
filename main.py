12173b12a9f4cde2498ca97b089403ba144a1544
Arthur Pereira <arthur.pereira@bhs.com.br>
2022-01-19 22:16:50 -0300

    added transform process

--- main.py
+++ main.py
@@ -7,10 +7,37 @@ from datetime import datetime
 import datetime
 import sqlite3
 
-# extract
+## transform ##
+def isValid(df: pd.DataFrame): 
+    if df.empty: # verifica se dados foram retornados ou não
+        print('Não foram encontradas músicas nas últimas 24 horas.')
+        return False
+
+    if pd.Series(df['played_at']).is_unique: # verifica se há chaves primárias repetidas
+        pass
+    else:
+        raise Exception('Há PK repetidas.')
+
+    if df.isnull().values.any(): # verifica se há valores vazios ou nulos
+        raise Exception('Há valores vazios/ nulos.')
+
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df['date_played'].tolist()
+
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+            raise Exception('Há músicas que não são das últimas 24 horas.')
+    
+    return True
+
+
+
+## extract ##
 database = 'sqlite:///spotify_tracks'
 user_id = 'vnihu9uaue7nfbjh80kpp1mug'
-token = 'BQCyZ7JXaS2-LqSX9sxZ-liYuH1cqCwd2jGR9rSS6oSps1L-NUnW7c2x5bQq5RpV7chXDAk9ajwGaPOHel3HYnEFoSz4RGeNR8855Aq3PCVGQtu-foJIP2XtBphTLeFFpIIj43r7jQtR11gQxYCL5xVTnuFIFspN0U-CrYJ1'
+token = 'BQDu5XnG0O5AEBukUNxKfYMVY2EjZ6HStG1iFuNQrrgD4jojvc-U8xyQFJTbZJR4xrfiFyyuBGN4maVwC8D8GCsqbZi3jKYouMaJpQvGx_9QqFC0-YCIwKLUZ59Yhy5wweoDGCYhg33MUYtnqFYkR1Y_AiALA2Lm6MRdacXl'
 
 # a api precisa de um parâmetro em milisegundos unix para determinar o tempo na qual a requisição vai rodar
 today = datetime.datetime.today()
@@ -31,20 +58,27 @@ song_names = []
 artist_names = []
 release_dates = []
 played_at = []
+date_played = []
 
 for song in data['items']:
     song_names.append(song['track']['name'])
     artist_names.append(song['track']['album']['artists'][0]['name'])
     release_dates.append(song['track']['album']['release_date'])
     played_at.append(song['played_at'])
+    date_played.append(song['played_at'][0:10])
 
     song_dict = {
         'song_name': song_names,
         'artist_name': artist_names,
         'release_date': release_dates,
-        'played_at': played_at 
+        'played_at': played_at,
+        'date_played': date_played
     }
 
-df = pd.DataFrame(song_dict, columns=['song_name', 'release_date', 'artist_name', 'played_at'])
+df = pd.DataFrame(song_dict, columns=['song_name', 'release_date', 'artist_name', 'played_at', 'date_played'])
+
+## validação ##
+if isValid(df):
+    print('Processo de validação concluído. Dados autenticados.')
 
 print(df)
